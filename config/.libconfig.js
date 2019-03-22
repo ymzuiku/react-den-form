086ffd6620ce5840bbdfb1ca3f52cdc35dcb614e
梁柱 <liangzhu@77ircloud.com>
2019-03-22 11:46:32 +0800

    fix: 更新github url

--- config/.libconfig.js
+++ config/.libconfig.js
@@ -3,5 +3,5 @@ module.exports = {
   dontLib: [], // dont babel files or dirs
   copy: ['README.md', 'README-EN.md', 'README-CN.md'], // only copy files or dirs
   delete: [], // after copy builded, delete files
-  gitURL: 'github.com/ymzuiku/react-project-gui', // git URL, like: 'github.com/ymzuiku/react-project-gui'
+  gitURL: 'github.com/ymzuiku/react-den-form',
 };
