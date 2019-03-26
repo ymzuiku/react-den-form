18776482790abda800f2174ff4e9f173bed7d94d
梁柱 <liangzhu@77ircloud.com>
2019-03-26 19:40:02 +0800

    fix: 去除 react 版本

--- form-example/config/.libconfig.js
+++ form-example/config/.libconfig.js
@@ -3,16 +3,14 @@ module.exports = {
   dontLib: [], // dont babel files or dirs
   copy: {
     'src/packages/react-den-form': '../src',
-    'dist': '../lib',
+    dist: '../lib',
     'dist/package.json': '../package.json',
   },
   delete: ['dist', '../lib/package.json'], // after copy builded, delete files
   package: {
-    "main": "lib/index.js",
-    "types": "src/index.d.ts",
-    "dependencies": {
-      "react": "^16.8.4"
-    },
+    main: 'lib/index.js',
+    types: 'src/index.d.ts',
+    dependencies: {},
   },
   gitURL: 'github.com/ymzuiku/react-den-form',
 };
